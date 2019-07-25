58ed9c78c24cea21529866a8391551febdcdb40f
Rohit Bhattacharya <rohit.bhattachar@gmail.com>
2019-07-25 16:45:55 -0400

    Implemented likelihood function

--- ananke/models/linear_gaussian_sem.py
+++ ananke/models/linear_gaussian_sem.py
@@ -8,33 +8,57 @@ import functools
 import autograd.numpy as anp
 from autograd import grad, hessian
 from scipy.optimize import minimize
+import numpy as np
+import copy
+
 
 class LinearGaussianSEM:
 
-    def __init__(self, graph, B=None, omega=None):
+    def __init__(self, graph):
         """
         Constructor
 
         :param graph: graph (ADMG) corresponding to the linear Gaussian SEM.
-        :param B: matrix of coefficients for the effect of each variable on another (should be lower triangular
-                  to maintain acyclicity).
-        :param omega: matrix of coefficients representing covariance between error terms of the SEM
-                      should be positive definite).
         """
 
         self.graph = graph
-        self.B = B
-        self.omega = omega
+        # for a linear Gaussian SEM each edge is a parameter
+        self.n_params = len(graph.di_edges) + len(graph.bi_edges)
 
-        # a mapping from vertex to index for easy referencing of matrix entries
-        vertex_index = {v: i for (i, v) in enumerate(self.graph.vertices)}
+        # TODO: check the size of these matrices to be d by d + lower triangular B + positive semi-definite omega
+        # TODO: throw an exception if not
+        self.X = None  # data matrix
+        self.S = None  # sample covariance
+        self.B = None  # direct edge coefficients
+        self.omega = None  # correlation of errors
+        self.vertex_index_map = {}
+        self.edge_param_map = {}
 
-    def fit(self, data):
+    def likelihood(self, X):
+        """
+        Calculate likelihood of the data given the model.
+
+        :param X: a N x M dimensional data matrix.
+        :return: a float corresponding to the likelihood.
         """
 
-        :param data: Fit the model to data -- an N x M dimensional matrix.
+        # first check if the model has been fit otherwise throw an error
+        if not self.B:
+            raise AssertionError("Model must be fit before likelihood can be calculated.")
+
+        n, d = X.shape
+        S = np.cov(X.T)
+        eye_inv_beta = np.linalg.inv(np.eye(d) - self.B)
+        sigma = np.dot(eye_inv_beta, np.dot(self.omega, eye_inv_beta.T))
+        return -(n/2) * (np.log(np.linalg.det(sigma)) - np.trace(np.dot(np.linalg.inv(sigma), S)))
+
+    def fit(self, X):
+        """
+
+        :param X: Fit the model to X -- a N x M dimensional data matrix.
         :return: None.
         """
 
-        pass
+        self.X = np.copy(X)
+        self.S = np.cov(X.T)
 
--- tests/models/test_linear_gaussian_sem.py
+++ tests/models/test_linear_gaussian_sem.py
@@ -2,13 +2,40 @@ import unittest
 
 from ananke.graphs import ADMG
 from ananke.models import LinearGaussianSEM
+import numpy as np
+
 
 class TestLinearGaussianSEM(unittest.TestCase):
 
-    def test_model(self):
+    def test_verma_model(self):
 
-        pass
+        vertices = ["A", "B", "C", "D"]
+        di_edges = [("A", "B"), ("B", "C"), ("C", "D")]
+        bi_edges = [("B", "D")]
+        G = ADMG(vertices, di_edges=di_edges, bi_edges=bi_edges)
+        model = LinearGaussianSEM(G)
+        self.assertEqual(4, model.n_params)
 
+        # generate data from an ADMG A->B->C->D B<->D and try to fit
+        N = 2000
+        dim = 4
+
+        omega = np.array([[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 1, 0],
+                          [0, 0, 0, 1]])
+
+        beta = np.array([[0, 0, 0, 0],
+                         [3, 0, 0, 0],
+                         [0, -1, 0, 0],
+                         [0, 3, 2.5, 0]])
+
+        true_sigma = np.linalg.inv(np.eye(dim) - beta) @ omega @ np.linalg.inv((np.eye(dim) - beta).T)
+        data = np.random.multivariate_normal([0] * dim, true_sigma, size=N)
+
+        # first test that without fitting you can't compute likelihood
+        with self.assertRaises(AssertionError):
+            model.likelihood(data)
 
 if __name__ == '__main__':
     unittest.main()
