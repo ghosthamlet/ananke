85db429f7d0a2f06d86f1ef32b4590c10ee1ed59
Rohit Bhattacharya <rohit.bhattachar@gmail.com>
2020-04-29 17:56:13 -0400

    Setup file edits

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ requirements = [
 ]
 
 setup(name='ananke-causal',
-      version='0.1.4',
+      version='0.1.5',
       description='Causal inference with graphical models',
-      long_description='Ananke, named for the Greek primordial goddess of necessity and causality, is a python package for causal inference using the language of graphical models.',
+      long_description=('Ananke, named for the Greek primordial goddess of necessity and causality,' +
+                        'is a python package for causal inference using the language of graphical models.\n\n' +
+                        'Documentation: https://ananke.readthedocs.io/en/latest/index.html'),
       url='https://gitlab.com/causal/ananke',
       author='See AUTHORS',
       author_email='rbhatta8@jhu.edu',
